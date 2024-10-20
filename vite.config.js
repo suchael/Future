5b75b43676db135cdea23984a6456b862d1353c7
suchael <suchael8@gmail.com>
2024-10-21 00:06:54 +0100

    deployment error fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // other configurations can go here
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"], // Add any other external dependencies if needed
+    },
+    sourcemap: true, // Optional: enables sourcemaps for easier debugging
+  },
 });
